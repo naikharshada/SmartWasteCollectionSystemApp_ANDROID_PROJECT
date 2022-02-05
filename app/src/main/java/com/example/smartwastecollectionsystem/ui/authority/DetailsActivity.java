0d4b4c6abebf87ada345df703f28d8c5441b4abc
hp <naikharshada2002@gmail.com>
2022-02-05 23:07:36 +0530

    Smart-Waste-Collection-System App

--- app/src/main/java/com/example/smartwastecollectionsystem/ui/authority/DetailsActivity.java
+++ app/src/main/java/com/example/smartwastecollectionsystem/ui/authority/DetailsActivity.java
@@ -1,16 +1,39 @@
 package com.example.smartwastecollectionsystem.ui.authority;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.smartwastecollectionsystem.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        changeStatusBarColor();
+        
+
+
     }
+
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
+        }
+    }
+
+
 }
