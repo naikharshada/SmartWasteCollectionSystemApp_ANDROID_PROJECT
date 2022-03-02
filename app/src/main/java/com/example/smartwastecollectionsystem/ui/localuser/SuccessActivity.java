4146b3efcf2f94e4049cc8d2facda0c10d507f5d
hp <naikharshada2002@gmail.com>
2022-03-02 14:40:57 +0530

    Smart-Waste-Collection-System App

---
+++ app/src/main/java/com/example/smartwastecollectionsystem/ui/localuser/SuccessActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartwastecollectionsystem.ui.localuser;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartwastecollectionsystem.R;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
