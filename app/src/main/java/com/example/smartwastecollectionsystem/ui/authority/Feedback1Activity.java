3339eb59be085a493b55e8de71dd4d5888ae957a
hp <naikharshada2002@gmail.com>
2022-03-25 00:09:44 +0530

    Smart-Waste-Collection-System App

--- app/src/main/java/com/example/smartwastecollectionsystem/ui/authority/Feedback1Activity.java
+++ app/src/main/java/com/example/smartwastecollectionsystem/ui/authority/Feedback1Activity.java
@@ -1,16 +1,47 @@
 package com.example.smartwastecollectionsystem.ui.authority;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartwastecollectionsystem.R;
 
 public class Feedback1Activity extends AppCompatActivity {
 
+    private ImageView backclick;
+    private TextView f_email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback1);
+
+
+        backclick = findViewById(R.id.back_imgclick);
+        f_email = findViewById(R.id.feedback_email_);
+
+
+        backclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Feedback1Activity.this, DetailsActivity.class));
+            }
+
+        });
+
+        f_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open email using client intent
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "swcs@gmail.com", null));
+                startActivity(Intent.createChooser(emailIntent, "Send mail"));
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/smartwastecollectionsystem/ui/localuser/FeedbackActivity.java
+++ app/src/main/java/com/example/smartwastecollectionsystem/ui/localuser/FeedbackActivity.java
@@ -1,6 +1,11 @@
 package com.example.smartwastecollectionsystem.ui.localuser;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +13,31 @@ import com.example.smartwastecollectionsystem.R;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    private ImageView backtoclick;
+    private TextView femail_;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        backtoclick = findViewById(R.id.back_imgC);
+        femail_ = findViewById(R.id.feedback_email);
+
+        backtoclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FeedbackActivity.this, ClickPictureActivity.class));
+            }
+        });
+
+        femail_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open email using client intent
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "swcs@gmail.com", null));
+                startActivity(Intent.createChooser(emailIntent, "Send mail"));
+            }
+        });
     }
 }
